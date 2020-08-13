6a8c7a81fc0e2b46d68853d55ef89e04cc323a61
sahrul6121 <sahrulramadan6121@gmail.com>
2020-08-13 18:45:58 +0700

    Set up vuexy

---
+++ database/migrations/2020_08_13_112738_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('store_id');
+            $table->bigInteger('creator_id');
+            $table->bigInteger('category_id');
+            $table->string('name');
+            $table->string('image');
+            $table->bigInteger('price');
+            $table->integer('stock');
+            $table->string('discount_percentage')->nullable();
+            $table->boolean('is_available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
